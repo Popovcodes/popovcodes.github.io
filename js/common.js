0552bd862138206c717b5303387fb5a17e1e20d2
Popovcodes <31511690+Popovcodes@users.noreply.github.com>
2017-09-05 20:17:15 +0200

    base

--- js/common.js
+++ js/common.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		var st = $(this).scrollTop();
 		$('.parallax_bg').css({
-			'-webkit-transform' : 'translate(0px,  '+ st/1.5 +'px)',
-			'-ms-transform' : 'translate(0px,  '+ st/1.5 +'px)',
-			'transform' : 'translate(0px,  '+ st/1.5 +'px)'
+			'-webkit-transform' : 'translate(0px,  '+ st/2 +'px)',
+			'-ms-transform' : 'translate(0px,  '+ st/2 +'px)',
+			'transform' : 'translate(0px,  '+ st/2 +'px)'
 		});
 	});
 });
