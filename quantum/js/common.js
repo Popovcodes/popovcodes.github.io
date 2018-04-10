59a7f64a0f750f93bc162813d617ba18e3761231
Popovcodes <31511690+Popovcodes@users.noreply.github.com>
2018-04-10 22:16:50 +0200

    qwedw

--- quantum/js/common.js
+++ quantum/js/common.js
@@ -28,4 +28,44 @@ $(document).ready(function(){
     // wow.js animation  http://mynameismatthieu.com/WOW/
     new WOW().init();
   };
+
+  //sticky_menu
+    // var menuHeight = $('.menu_logo_line').outerHeight(true);  
+    // var Hoffset = menuHeight+'px';
+    var stickyLogo = '<a href="index.html" class="sticky-logo"><img src="img/logo-static.png" alt="image"></a>'
+
+    var stickyStyleSet = function(){
+      $('.headerNav').addClass('sticky');
+    }
+
+    var stickyLogoSet = function(){
+      if($('.sticky-logo').length) {
+        $.noop;
+      } else {
+        $('.headerNav_list').before(stickyLogo);
+        $('.sticky-logo').animate({opacity: '1'}, 1000);
+      }
+    }
+
+    var stickyNav = function(){
+      //var headerNavOffset = $('.headerNav').offset().top;
+      if(document.documentElement.clientWidth > 620) {
+        var documentScroll = $(this).scrollTop();
+         if(documentScroll >= 50) {
+          $('.headerNav').addClass('sticky');
+          stickyLogoSet();
+        } else {
+          $('.headerNav').removeClass('sticky');
+          $('.sticky-logo').remove();
+        }
+      } else {
+        //$('.headerNav').css({'position': 'relative'});
+      }
+    }
+    stickyNav();
+    $(window).scroll(stickyNav).resize(stickyNav);
+
+
+
+
 });
