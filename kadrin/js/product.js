e9a99d220aa0efafe78f9ef6bd9afb686c8033d8
Popovcodes <31511690+Popovcodes@users.noreply.github.com>
2018-06-16 14:21:14 +0200

    weafa

--- kadrin/js/product.js
+++ kadrin/js/product.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
+  //slick.js slider   http://kenwheeler.github.io/slick/
+  $('#js-product-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true
+  });
+
+  //features slider
+  $('#js-feature-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true
+  });
   
+  //popup images
+  $('.popup').fancybox({});
 });
