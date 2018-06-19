35b61599923263ca78623591e2726bf715014bf8
Popovcodes <31511690+Popovcodes@users.noreply.github.com>
2018-06-19 09:46:33 +0200

    asd

---
+++ kadrin/js/material-googlemap.js
@@ -0,0 +1,33 @@
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 10,
+        streetViewControl: false,
+        draggable: false,
+        mapTypeControl: false,
+
+        // The latitude and longitude to center the map (always required)
+        center: new google.maps.LatLng(51.9962683,85.6579965,13), 
+
+    };
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('material-map');
+
+    // Create the Google Map using our element and options defined above
+    var map1 = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(51.9962683,85.6579965,13),
+        map: map1,
+       /* icon: image,*/
+
+    });
+}
