befc71f4517df026ddf56cb2c8429e0ec248ed39
Popovcodes <31511690+Popovcodes@users.noreply.github.com>
2018-10-13 13:52:13 +0300

    asd

--- jj/js/index.js
+++ jj/js/index.js
@@ -87,7 +87,7 @@ var parallax = function(){
         $(this).find('img').css({"left": thisVisible/coefX, "bottom": thisVisible/coefY, "transform": "rotate("+ rotate*2.5 +"deg)"});
       };
     });
-/*
+
     $('.eq-img-cont').each(function(){
       var eqItemPosition = $(this).offset().top;
       var eqItemVisible = scrolledToWindowBottom - eqItemPosition;
@@ -101,7 +101,7 @@ var parallax = function(){
       if(featItemVisible > 0) {
         $(this).parents('li').css({"transform": "translate(0%, "+ -featItemVisible/50+"%)"});
       };
-    });*/
+    });
   }
 };
 
