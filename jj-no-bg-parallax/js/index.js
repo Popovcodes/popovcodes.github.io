711992545b1071da8d4040a939df02fa31e87b27
Popovcodes <31511690+Popovcodes@users.noreply.github.com>
2018-10-16 15:54:45 +0300

    ыфцасф

--- jj-no-bg-parallax/js/index.js
+++ jj-no-bg-parallax/js/index.js
@@ -79,7 +79,7 @@ var parallax = function(){
       $('#blue-line').css({"transform": "rotate(8deg) translate(0%, "+ -blueLineVisible/4+"px)"});
     };
 	*/
-/*
+
     // flying objects
     $('.f-1, .f-6').each(function(){
       var thisPosition = $(this).offset().top;
@@ -120,7 +120,7 @@ var parallax = function(){
         $(this).find('img').css({"left": thisVisible/3});
       };
     });
-	*/
+
 /*
     $('.f-6').each(function(){
       var thisPosition = $(this).offset().top;
